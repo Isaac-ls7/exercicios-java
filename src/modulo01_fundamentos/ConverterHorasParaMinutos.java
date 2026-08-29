cff47b31c643d4889c5ea1d53fd585c3e04ba0ac
Isaac Lima Santos <limaisaac602@gmail.com>
2026-08-29 15:29:25 -0300

    terceira questão modulo01_fundamentos

---
+++ src/modulo01_fundamentos/ConverterHorasParaMinutos.java
@@ -0,0 +1,17 @@
+
+package modulo01_fundamentos;
+
+
+public class ConverterHorasParaMinutos {
+
+   
+    public static void main(String[] args) {
+        
+        double horas = 2;
+        
+        double minutos  =  horas * 60;
+        
+        System.out.printf("A quantidade de horas em minutos é: %.2f\n",minutos);
+    }
+
+}
