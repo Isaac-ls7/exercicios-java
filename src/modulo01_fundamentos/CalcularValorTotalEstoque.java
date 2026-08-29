d5989d0027e724299c81059012759e19190e7739
Isaac Lima Santos <limaisaac602@gmail.com>
2026-08-29 14:55:42 -0300

    atualizando

--- src/modulo01_fundamentos/CalcularValorTotalEstoque.java
+++ src/modulo01_fundamentos/CalcularValorTotalEstoque.java
@@ -1,12 +1,21 @@
-
 package modulo01_fundamentos;
-
 
 public class CalcularValorTotalEstoque {
 
-   
     public static void main(String[] args) {
-        
-    }
+        // Declaração de variáveis
+        String nomePeca = "Tela Frontal Moto G32";
+        int quantidade = 8;
+        double precoUnitario = 120.50;
 
+        // Cálculo do valor total
+        double valorTotal = quantidade * precoUnitario;
+
+        // Exibição dos resultados
+        System.out.println("--- RELATÓRIO DE ESTOQUE ---");
+        System.out.println("Peça: " + nomePeca);
+        System.out.println("Quantidade em estoque: " + quantidade);
+        System.out.printf("Preço unitário: R$ %.2f\n", precoUnitario);
+        System.out.printf("Valor total em estoque: R$ %.2f\n", valorTotal);
+    }
 }
